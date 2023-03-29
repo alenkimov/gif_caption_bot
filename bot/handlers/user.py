35090da68a27e0e7d08d2de6abf306669adaf6d1
Alen <alen.kimov@gmail.com>
2023-03-29 15:12:39 +0300

    feat: теперь можно отредактировать подпись и бот пришлет новую гифку

--- bot/handlers/user.py
+++ bot/handlers/user.py
@@ -3,7 +3,7 @@ import os
 
 from aiogram import F, Router, Bot
 from aiogram.filters import Command
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message, FSInputFile, Update
 
 from bot.utils import watermarked_mp4
 from bot.definitions import TEMP_DIR
@@ -14,10 +14,12 @@ router = Router()
 @router.message(Command('help'))
 async def cmd_help(message: Message):
     await message.answer(f'Отправь мне гифку и я добавлю на нее твое имя пользователя!\n'
-                         f'Или отправь мне гифку с подписью!')
+                         f'Или отправь мне гифку с подписью!\n'
+                         f'Измени подпись у гифки и я отправлю тебе новую!')
 
 
 @router.message(F.animation)
+@router.edited_message(F.animation)
 async def photo_handler(message: Message, bot: Bot):
     telegram_handle = f'@{message.from_user.username}'
     mp4_filepath = TEMP_DIR / f'{telegram_handle}-{secrets.token_urlsafe(8)}.mp4'
