94e6c7ede7d1063cb2805c2eb6db39f84b4c1a91
Alen <alen.kimov@gmail.com>
2023-03-29 10:28:11 +0300

    feat: теперь текст отображается нормально
    - Размер шрифта теперь зависит от ширины изображения.
    - Длинный текст теперь переносится на новые строки.

--- bot/utils.py
+++ bot/utils.py
@@ -1,9 +1,11 @@
 import os
 from contextlib import contextmanager
 from pathlib import Path
+from pprint import pprint
 
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 
+from bot.logger import logger
 
 @contextmanager
 def watermarked_mp4(path: Path, watermark_text: str) -> Path:
@@ -13,15 +15,35 @@ def watermarked_mp4(path: Path, watermark_text: str) -> Path:
     mp4 = VideoFileClip(path.as_posix())
     output_video_path = path.parent / f'{path.stem}-output.mp4'
 
-    watermark = (
-        TextClip(txt=watermark_text,
-                 font='Arial-Black', fontsize=40, color='white',
-                 stroke_color='black', stroke_width=1)
-        .set_position('bottom')
-        .set_duration(mp4.duration)
-    )
+    # Сборка текста-вотермарки
+    fontsize = mp4.w // 10
+    logger.warning(f'mp4.w: {mp4.w}')
+    logger.warning(f'fontsize: {fontsize}')
 
-    watermarked_mp4 = CompositeVideoClip([mp4, watermark])
-    watermarked_mp4.write_videofile(output_video_path.as_posix())
+    # Перенос строк для длинного текста
+    watermark_text_parts = watermark_text.split()
+    max_len = mp4.w // (fontsize * 1.2)
+    len_sum = 0
+    for i in range(len(watermark_text_parts) - 1):
+        len_sum += len(watermark_text_parts[i])
+        if len_sum > max_len:
+            len_sum = 0
+            watermark_text_parts[i] += '\n'
+    watermark_text = ' '.join(watermark_text_parts)
+
+    text_clip_params = {
+        'txt': watermark_text,
+        'font': 'Arial-Black',
+        'fontsize': fontsize,
+        'color': 'white',
+    }
+    if fontsize > 30:  # Текст будет без обводки, если размер шрифта слишком маленький
+        text_clip_params.update({'stroke_color': 'black', 'stroke_width': fontsize // 20})
+    pprint(text_clip_params)
+    watermark = TextClip(**text_clip_params).set_position('bottom').set_duration(mp4.duration)
+
+    # Сохранение видео по пути output_video_path
+    CompositeVideoClip([mp4, watermark]).write_videofile(output_video_path.as_posix())
+
     yield output_video_path
     os.remove(output_video_path)
