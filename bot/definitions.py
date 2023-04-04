33be69008d0b402807fbce4fc1d951a006a4c79d
Alen <alen.kimov@gmail.com>
2023-04-04 18:17:34 +0300

    feat: логгер больше не записывает лог в файл за ненадобностью

--- bot/definitions.py
+++ bot/definitions.py
@@ -4,8 +4,6 @@ from os import makedirs
 BASE_DIR = Path(__file__).resolve().parent.parent
 DOT_ENV_FILEPATH = BASE_DIR / '.env'
 TEMP_DIR = BASE_DIR / '.temp'
-LOG_DIR = BASE_DIR / 'log'
 
 
 makedirs(TEMP_DIR, exist_ok=True)
-makedirs(LOG_DIR, exist_ok=True)
--- bot/logger.py
+++ bot/logger.py
@@ -5,12 +5,9 @@ import sys
 
 from loguru import logger
 
-from bot.definitions import LOG_DIR
-
 
 DEBUG = True
 
-FILE_LOG_FORMAT = "<white>{time:YYYY-MM-DD HH:mm:ss}</white> | <level>{level: <8}</level> | <white>{message}</white>"
 CONSOLE_LOG_FORMAT = "<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <white>{message}</white>"
 
 
@@ -33,10 +30,7 @@ class InterceptHandler(logging.Handler):
 
 def setup(debug: bool):
     logger.remove()
-    log_file_name = f'{datetime.now().strftime("%d-%m-%Y")}.log'
-    log_file_path = LOG_DIR / log_file_name
     logging.basicConfig(handlers=[InterceptHandler()], level=logging.INFO)
-    logger.add(log_file_path, format=FILE_LOG_FORMAT, level="DEBUG", rotation='1 day')
     logger.add(sys.stderr, colorize=True, format=CONSOLE_LOG_FORMAT, level='DEBUG' if debug else 'INFO')
 
 
